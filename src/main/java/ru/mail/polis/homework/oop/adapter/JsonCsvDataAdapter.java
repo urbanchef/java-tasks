687127d6256224923f65e3065e18a091f0897607
Roman Muschinsky <roman.muschinsky@vk.team>
2023-03-23 21:04:01 +0700

    adapter task

--- src/main/java/ru/mail/polis/homework/oop/adapter/JsonCsvDataAdapter.java
+++ src/main/java/ru/mail/polis/homework/oop/adapter/JsonCsvDataAdapter.java
@@ -4,5 +4,22 @@ package ru.mail.polis.homework.oop.adapter;
  * После имплементации раскоментировать тест в ru.mail.polis.homework.oop.adapter.ProductPriceParsServiceTest
  * для проверки правильной имплементации
  */
-public class JsonCsvDataAdapter {
+public class JsonCsvDataAdapter extends CsvData {
+
+    public JsonCsvDataAdapter(String text) {
+        super(text);
+    }
+
+    public String convertJsonToCsv(String text) {
+        String placeHolder = text.replace(':', ',').replace("\"", "");
+        StringBuilder sb = new StringBuilder(placeHolder);
+        sb.deleteCharAt(0);
+        sb.deleteCharAt(sb.toString().length() - 1);
+        return sb.toString();
+    }
+
+    @Override
+    public String getText() {
+        return convertJsonToCsv(super.getText());
+    }
 }
