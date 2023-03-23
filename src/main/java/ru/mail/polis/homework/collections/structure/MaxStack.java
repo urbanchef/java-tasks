120124d38f2647966e32bd9aac205c31a10cbe31
Roman Muschinsky <roman.muschinsky@vk.team>
2023-03-23 21:06:41 +0700

    MaxStack task

--- src/main/java/ru/mail/polis/homework/collections/structure/MaxStack.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/MaxStack.java
@@ -1,5 +1,8 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.Objects;
 import java.util.Stack;
 
 /**
@@ -9,9 +12,38 @@ import java.util.Stack;
  * храниться в Stack. Отрабатывать метод должен за О(1).
  */
 public class MaxStack extends Stack<Integer> {
+    private ArrayList<Integer> maxValues = new ArrayList<>();
+    private Integer currentMaxValue = 0;
 
-    public Integer getMaxValue() {
-        return 0;
+    public MaxStack() {
+        super();
     }
 
+    public Integer getMaxValue() {
+        if (super.isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return currentMaxValue;
+    }
+
+    @Override
+    public Integer push(Integer item) {
+        if (item >= currentMaxValue) {
+            currentMaxValue = item;
+            maxValues.add(currentMaxValue);
+        }
+        return super.push(item);
+    }
+
+    @Override
+    public Integer pop() {
+        int currentValue = super.pop();
+        if (Objects.equals(currentValue, currentMaxValue)) {
+            maxValues.remove(maxValues.size() - 1);
+            if (!maxValues.isEmpty()) {
+                currentMaxValue = maxValues.get(maxValues.size() - 1);
+            }
+        }
+        return currentValue;
+    }
 }
