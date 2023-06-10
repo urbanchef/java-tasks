c197ea7a50df50c708d6f87ec2fbd24c5741b0b3
Roman Muschinsky <roman.muschinsky@vk.team>
2023-06-10 22:01:45 +0300

    CustomDictionary update

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
@@ -1,7 +1,11 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Задание оценивается в 4 тугрика.
@@ -11,33 +15,58 @@ import java.util.List;
  */
 public class CustomDictionary {
 
+    private Map<String, List<String>> stringsStorage = new HashMap<>();
+
+    private String prepareString(String value) {
+        return value.toLowerCase().trim().replaceAll("\\s+", "");
+    }
+
+    private String keyString(String value) {
+        String preparedString = prepareString(value);
+        char[] charString = preparedString.toCharArray();
+        Arrays.sort(charString);
+
+        return Arrays.toString(charString);
+
+    }
+
     /**
      * Сохранить строку в структуру данных
+     *
      * @param value - передаваемая строка
      * @return - успешно сохранили строку или нет.
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - [O(value.length())]
      */
     public boolean add(String value) {
-        return false;
+        if (value == null || value.isEmpty() || value.isBlank()) {
+            return false;
+        }
+
+        String preparedKeyString = keyString(value);
+        List<String> stringValue = stringsStorage.getOrDefault(preparedKeyString, new ArrayList<>());
+
+        return stringValue.add(value);
     }
 
     /**
      * Проверяем, хранится ли такая строка уже у нас
+     *
      * @param value - передаваемая строка
      * @return - есть такая строка или нет в нашей структуре
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - [O(value.length())]
      */
     public boolean contains(String value) {
-        return false;
+        return stringsStorage.containsKey(keyString(value));
     }
 
     /**
      * Удаляем сохраненную строку если она есть
+     *
      * @param value - какую строку мы хотим удалить
      * @return - true если удалили, false - если такой строки нет
-     *
+     * <p>
      * Сложность - []
      */
     public boolean remove(String value) {
@@ -51,7 +80,7 @@ public class CustomDictionary {
      * сохраняем строки ["aaa", "aBa", "baa", "aaB"]
      * При поиске по строке "AAb" нам должен вернуться следующий
      * список: ["aBa","baa","aaB"]
-     *
+     * <p>
      * сохраняем строки ["aaa", "aAa", "a"]
      * поиск "aaaa"
      * результат: []
@@ -60,7 +89,7 @@ public class CustomDictionary {
      *
      * @return - список слов которые состоят из тех же букв, что и передаваемая
      * строка.
-     *
+     * <p>
      * Сложность - []
      */
     public List<String> getSimilarWords(String value) {
@@ -69,8 +98,9 @@ public class CustomDictionary {
 
     /**
      * Колл-во хранимых строк.
-     * @return - Колл-во хранимых строк.
      *
+     * @return - Колл-во хранимых строк.
+     * <p>
      * Сложность - []
      */
     public int size() {
