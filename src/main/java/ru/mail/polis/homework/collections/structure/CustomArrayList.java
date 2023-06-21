9ac89bba734577ed1e7c63fe6686d2483a2840fe
Roman Mushchinskiy <rmushchinskiy@openwaygroup.com>
2023-06-22 00:41:04 +0300

    CustomArrayList part 1

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomArrayList.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomArrayList.java
@@ -1,9 +1,11 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Objects;
 
 /**
  * Необходимо реализовать свой ArrayList (динамический массив).
@@ -12,19 +14,43 @@ import java.util.ListIterator;
  * Задание оценивается в 10 тугриков
  */
 public class CustomArrayList<E> implements List<E> {
+
+    private int size;
+    private static final int DEFAULT_INITIAL_CAPACITY = 10;
+    private Object[] elementData;
+
+    private static final Object[] EMPTY_ELEMENTDATA = {};
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+
+    public CustomArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            this.elementData = new Object[initialCapacity];
+        } else if (initialCapacity == 0) {
+            this.elementData = EMPTY_ELEMENTDATA;
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: "+
+                    initialCapacity);
+        }
+
+    }
+
+    public CustomArrayList() {
+        this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) >= 0;
     }
 
     @Override
@@ -34,17 +60,41 @@ public class CustomArrayList<E> implements List<E> {
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        return Arrays.copyOf(elementData, size);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public <T> T[] toArray(T[] a) {
-        return null;
+        if (a.length < size) {
+            return (T[]) Arrays.copyOf(elementData, a.length);
+        }
+        System.arraycopy(elementData, 0, a, 0, size);
+        return a;
     }
 
     @Override
     public boolean add(E e) {
-        return false;
+        if (size == elementData.length) {
+            elementData = grow();
+        }
+        elementData[size] = e;
+        size++;
+        return true;
+    }
+
+    private Object[] grow() {
+        return grow(size + 1);
+    }
+
+    private Object[] grow(int minCapacity) {
+        int oldCapacity = elementData.length;
+        if (oldCapacity > 0 || elementData != DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
+            int newCapacity = oldCapacity + oldCapacity >> 1;
+            return elementData = Arrays.copyOf(elementData, newCapacity);
+        } else {
+            return elementData = new Object[Math.max(DEFAULT_INITIAL_CAPACITY, minCapacity)];
+        }
     }
 
     @Override
@@ -79,12 +129,14 @@ public class CustomArrayList<E> implements List<E> {
 
     @Override
     public void clear() {
-
+        elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+        size = 0;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public E get(int index) {
-        return null;
+        return (E) elementData[index];
     }
 
     @Override
@@ -104,12 +156,23 @@ public class CustomArrayList<E> implements List<E> {
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(o, elementData[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        int lastIndex = -1;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(o, elementData[i])) {
+                lastIndex = i;
+            }
+        }
+        return lastIndex;
     }
 
     @Override
